6a0029206d4fcbd86de62c87a95a191746ed227d
bulgakke <vvp835@yandex.ru>
2019-12-27 01:02:30 +0300

    Test::of_category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,13 @@
 class Test < ApplicationRecord
   has_many :questions
   has_and_belongs_to_many :users
+
+  def self.of_category(category_title)
+    to_return = []
+    category_id = Category.find_by(id: category_title)
+    self.where("category_id = ?", category_id).order(title: :desc).each do |test|
+      to_return << test.title
+    end
+    return to_return
+  end
 end
