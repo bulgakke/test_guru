b87cc5207beb0858ec6b8cb822892f3cca26d786
bulgakke <vvp835@yandex.ru>
2019-12-27 11:46:11 +0300

    Фикса метода отбора тестов

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :category
   has_many :questions
   has_and_belongs_to_many :users
 
   def self.of_category(category_title)
-    to_return = []
-    category_id = Category.find_by(id: category_title)
-    self.where("category_id = ?", category_id).order(title: :desc).each do |test|
-      to_return << test.title
-    end
-    return to_return
+    self.joins(:category).where("category_id = ?", category_title).order(title: :desc).pluck(:title)
   end
 end
--- db/migrate/20191226192847_add_test_taken_to_user.rb
+++ db/migrate/20191226192847_add_test_taken_to_user.rb
@@ -1,13 +1,5 @@
 class AddTestTakenToUser < ActiveRecord::Migration[6.0]
   def change
-    change_table :users do |t|
-
-    end
-
-    change_table :tests do |t|
-
-    end
-
     create_table :tests_users, id: false do |t|
       t.belongs_to :user
       t.belongs_to :test
