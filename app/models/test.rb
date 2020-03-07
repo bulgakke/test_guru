e80ebeef1b6e422458f14f2c763dbdc2f2f4eb5c
bulgakke <vvp835@yandex.ru>
2020-03-07 23:23:24 +0300

    Может существовать только один Тест с данным названием и уровнем

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,6 +6,7 @@ class Test < ApplicationRecord
 
   validates :title, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level, message: 'A test with this title already exists for this level' }
 
   def self.of_category(category_title)
     self.joins(:category).where("category_id = ?", category_title).order(title: :desc).pluck(:title)
