6f1dd9e0bd36beed8cf22880bb259ba736006e97
bulgakke <vvp835@yandex.ru>
2020-03-07 23:46:47 +0300

    У одного Вопроса может быть от 1-го до 4-х ответов.

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,11 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
+  validate :validates_number_of_answers
+
+  private
+  
+  def validates_number_of_answers
+    errors.add(:number_of_answers, 'Must be between 1 and 4') unless (1..4).include?(answers.size)
+  end
 end
