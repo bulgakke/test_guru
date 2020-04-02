2114ce9b9c7849ae03d6d142e1b6bb96820876cf
bulgakke <vvp835@yandex.ru>
2020-04-03 00:24:06 +0300

    Просмотр списка вопросов теста

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,18 @@
 class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[index]
+
+  def index
+    @questions = @test.questions
+    @questions_list = @questions.map do |question| 
+      question.body.concat("\n")
+    end
+    render inline: "<p>Test name: <%= @test.title %></p> <p>Test questions list: <%= @questions_list.join %> </p>"
+  end
+
+  private
+  
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 end
