2b8645fc0e1eccfb2902cf7c39f35a980d3f6bdf
bulgakke <vvp835@yandex.ru>
2020-04-07 01:30:02 +0300

    Перенёс футер в shared

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,7 +5,9 @@ class QuestionsController < ApplicationController
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-  def index #
+  def index
+    @test
+
     @questions = @test.questions 
     @questions_list = @questions.map do |question| 
       question.body.concat("\n")
